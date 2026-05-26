780e5fc55cb21468b80151827f2f2eab2a759d52
Hua Vuong <huavuongne@gmail.com>
2026-05-26 14:38:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ if __name__ == "__main__":
     game.run() 
     
 #-- CHÚ Ý --
-#1. Mỗi lần đẩy code lên nhớ ghi chú rõ đã Thêm/Sửa những gì.
-#2. Phải chú thích vai trò của mỗi đoạn mình viết để tiện cho việc giải thích + để thành viên khác đọc và hiểu.
+
+#1. Không đẩy đoạn code chưa hoàn thiện.
+
+#2. Mỗi lần đẩy code lên nhớ ghi chú rõ đã Thêm/Sửa những gì.
+
+#3. Phải chú thích vai trò của mỗi đoạn code và mỗi lớp.
+
+#4. Không được commit nội dung vớ vẩn vì thầy sẽ thấy!!!!!.
+
+
+
