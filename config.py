88137120a415835d6bb571654432e0cacd0b7492
HuaQuocVuong <huavuongne@gmail.com>
2026-06-13 00:37:57 +0700

    update

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ SCREEN_HEIGHT = 540  #1040  # Độ cao cửa sổ game 520
 SCREEN_WIDTH = 1280 # Độ rộng cửa sổ game -100 cho đến khi phù hợp
 SCREEN_HEIGHT  = 720 # Độ cao cửa sổ game -100 cho đến khi phù hợp
 
-SCREEN_WIDTH = 1920 # Độ rộng cửa sổ game -100 cho đến khi phù hợp
-SCREEN_HEIGHT  = 1040 # Độ cao cửa sổ game -100 cho đến khi phù hợp
+#Không xóa!!!!!!!
+#SCREEN_WIDTH = 1920 # Độ rộng cửa sổ game -100 cho đến khi phù hợp
+#SCREEN_HEIGHT  = 1040 # Độ cao cửa sổ game -100 cho đến khi phù hợp
 
 
 # Kích thước vùng nhìn của camera (vùng thế giới game được hiển thị trong game_surface)
